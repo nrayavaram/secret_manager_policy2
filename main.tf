7b3d9d91d9004ffb534319cac491609bf30e8948
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-06 11:47:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+provider "google" {
+  access_token = var.access_token
+}
 
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "secret-wf-google"
+
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
