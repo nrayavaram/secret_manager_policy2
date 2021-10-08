8f9df702de94380eecf9858494588289b5c2d41b
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-08 11:00:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,20 @@
 provider "google" {
   access_token = var.access_token
+  project      = "manifest-access-320809"
 }
 
 resource "google_secret_manager_secret" "secret-basic" {
-  secret_id = "secret-wf-google-tf"
-  project      = "manifest-access-320809"
-
+  secret_id = "secret"
 
   labels = {
-    label = "my-label2"
+    label = "my-label"
   }
 
   replication {
-    user_managed {
-      replicas {
+    
+     replicas {
         location = "us-central1"
       }
-      replicas {
-        location = "europe-west1"
-      }
-    }
+    
   }
 }
